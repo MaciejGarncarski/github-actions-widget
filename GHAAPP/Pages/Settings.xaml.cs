5c2b4c7a6af1698e67020944f975fb78d93c3b4f
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-07-12 23:14:07 +0200

    initial

---
+++ GHAAPP/Pages/Settings.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Windows.AppNotifications;
+using Microsoft.Windows.AppNotifications.Builder;
+
+namespace GHAAPP.Pages
+{
+    public sealed partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+        
+        private void RevealModeCheckbox_Changed(object sender, RoutedEventArgs e)
+        {
+            if (revealModeCheckBox.IsChecked == true)
+            {
+                passworBoxWithRevealmode.PasswordRevealMode = PasswordRevealMode.Visible;
+            }
+            else
+            {
+                passworBoxWithRevealmode.PasswordRevealMode = PasswordRevealMode.Hidden;
+            }
+        }
+
+        private void SavePATHButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppNotification notification = new AppNotificationBuilder()
+                .AddText("Welcome to WinUI 3 Gallery")
+                .AddText("Explore interactive samples and discover the power of modern Windows UI.")
+                .BuildNotification();
+
+            AppNotificationManager.Default.Show(notification);
+        }
+    }
+}
