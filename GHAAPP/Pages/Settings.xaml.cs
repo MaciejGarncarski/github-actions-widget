12ec167a6cfb97c3360f4c3d4433f61b1e5f6673
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-07-13 23:51:11 +0200

    feat: add credential manager

--- GHAAPP/Pages/Settings.xaml.cs
+++ GHAAPP/Pages/Settings.xaml.cs
@@ -1,17 +1,18 @@
-﻿using Microsoft.UI.Xaml;
+﻿using GHAAPP.Services;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.Windows.AppNotifications;
 using Microsoft.Windows.AppNotifications.Builder;
 
 namespace GHAAPP.Pages
 {
-    public sealed partial class SettingsPage : Page
+    public sealed partial class SettingsPage
     {
         public SettingsPage()
         {
             InitializeComponent();
         }
-        
+
         private void RevealModeCheckbox_Changed(object sender, RoutedEventArgs e)
         {
             if (revealModeCheckBox.IsChecked == true)
@@ -24,14 +25,33 @@ namespace GHAAPP.Pages
             }
         }
 
-        private void SavePATHButton_Click(object sender, RoutedEventArgs e)
+        private void SavePersonalAccessToken(object sender, RoutedEventArgs e)
         {
-            AppNotification notification = new AppNotificationBuilder()
-                .AddText("Welcome to WinUI 3 Gallery")
-                .AddText("Explore interactive samples and discover the power of modern Windows UI.")
+            AppNotification errorNotification = new AppNotificationBuilder()
+                .AddText("Invalid token provided.")
                 .BuildNotification();
 
-            AppNotificationManager.Default.Show(notification);
+            try
+            {
+                string token = passworBoxWithRevealmode.Password;
+
+                if (string.IsNullOrWhiteSpace(token))
+                {
+                    AppNotificationManager.Default.Show(errorNotification);
+                    return;
+                }
+
+                TokenStore.Save(token);
+                
+                AppNotification notification = new AppNotificationBuilder()
+                    .AddText("Token was saved.")
+                    .BuildNotification();
+                AppNotificationManager.Default.Show(notification);
+            }
+            catch 
+            {
+                AppNotificationManager.Default.Show(errorNotification);
+            }
         }
     }
 }
---
+++ GHAAPP/Services/TokenStore.cs
@@ -0,0 +1,26 @@
+﻿namespace GHAAPP.Services;
+public class TokenStore
+{
+    private static readonly string AppName = "GHAAPP";
+    private static readonly string UserName = "PersonalAccessToken";
+    public static void Save(string token)
+    {
+        CredentialManager.WriteCredential(AppName, UserName, token);
+    }
+    public static string? Read()
+    {
+        var credential = CredentialManager.ReadCredential(AppName);
+        
+        if (credential == null)
+        {
+            return null;
+        }
+
+        if (string.IsNullOrWhiteSpace(credential.Password))
+        {
+            return null;
+        }
+        
+        return credential.Password;
+    }
+}
