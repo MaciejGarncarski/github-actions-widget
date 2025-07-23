1468c3e50e9b68e3fe8f87ad803c092305cd88df
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-07-23 14:03:43 +0200

    chore: add z-index

--- src/features/cookie-banner/components/cookie-banner.tsx
+++ src/features/cookie-banner/components/cookie-banner.tsx
@@ -14,7 +14,7 @@ export const CookieBanner = ({ accepted }: { accepted: boolean }) => {
   if (!isVisible) return null;
 
   return (
-    <div className="border-white/30 fixed bottom-4 left-4 right-4 md:left-8 max-w-lg md:right-8 bg-white/20 backdrop-blur-xl rounded-lg p-4 flex flex-col md:flex-row items-center justify-between animate-slide-up">
+    <div className="border-white/30 fixed z-20 bottom-4 left-4 right-4 md:left-8 max-w-lg md:right-8 bg-white/20 backdrop-blur-xl rounded-lg p-4 flex flex-col md:flex-row items-center justify-between animate-slide-up">
       <p className="text-sm text-white">
         By using this app, you agree to our use of cookies.
       </p>
